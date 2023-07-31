5dd7a29a3d28214720c39103a6e11fa1fa2203bc
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-07-31 19:05:23 +0300

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -13,29 +13,7 @@ const ContactsPage = () => {
       <h2 className={css.title}>Where we are</h2>
       <div className={css.locations}>
         <ul className={css.list}>
-          <li className={css.item}>
-              <iframe 
-                className={css.map}
-                title="2750-412 Cascais" 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.9879235092894!2d-9.422983222975878!3d38.695121671767964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ec428dc1c9037%3A0x18ff353547b607bb!2zUi4gTHXDrXMgWGF2aWVyIFBhbG1laXJpbSA0LCAyNzUwLTQxMiBDYXNjYWlzLCDQn9C-0YDRgtGD0pHQsNC70ZbRjw!5e0!3m2!1suk!2sua!4v1690061275891!5m2!1suk!2sua" 
-                width="560" 
-                height="450" 
-                allowFullScreen="" 
-                loading="lazy" 
-                referrerPolicy="no-referrer-when-downgrade">
-              </iframe>
-              <div className={css.thumb}>
-                <img src={Cascais} alt='Cascais location' width="560" height="450" className={css.img}/>
-                <div className={css.thumb_overlay}>
-                  <p className={css.text}>
-                    R. Luís Xavier Palmeirim 4 <br/>
-                    2750-412 <br/>
-                    Cascais, Portugal
-                  </p>
-                </div>
-              </div>
-          </li>
-          <li className={css.item}>
+        <li className={css.item}>
               <div className={css.thumb}>
                 <img src={Lisboa} alt='Cascais location' width="560" height="450" className={css.img}/>
                 <div className={css.thumb_overlay}>
@@ -58,6 +36,29 @@ const ContactsPage = () => {
                 referrerPolicy="no-referrer-when-downgrade">
               </iframe>
           </li>
+          <li className={css.item}>
+              <iframe 
+                className={css.map}
+                title="2750-412 Cascais" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.9879235092894!2d-9.422983222975878!3d38.695121671767964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ec428dc1c9037%3A0x18ff353547b607bb!2zUi4gTHXDrXMgWGF2aWVyIFBhbG1laXJpbSA0LCAyNzUwLTQxMiBDYXNjYWlzLCDQn9C-0YDRgtGD0pHQsNC70ZbRjw!5e0!3m2!1suk!2sua!4v1690061275891!5m2!1suk!2sua" 
+                width="560" 
+                height="450" 
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade">
+              </iframe>
+              <div className={css.thumb}>
+                <img src={Cascais} alt='Cascais location' width="560" height="450" className={css.img}/>
+                <div className={css.thumb_overlay}>
+                  <p className={css.text}>
+                    R. Luís Xavier Palmeirim 4 <br/>
+                    2750-412 <br/>
+                    Cascais, Portugal
+                  </p>
+                </div>
+              </div>
+          </li>
+          
         </ul>
       </div>
       <h2 className={css.title}>Let's get in touch</h2>
